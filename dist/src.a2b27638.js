afe6a7d928f0f7d25ae74f2858e26d5f0b316ba5
celinaryan <cryan27@nd.edu>
2023-10-24 17:12:14 -0400

    fixed bug I found

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -1956,7 +1956,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56671" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57260" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
