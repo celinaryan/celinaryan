360a4cc1725c522a0c0c249c952210b30e112c34
celinaryan <cryan27@nd.edu>
2024-02-19 13:28:59 -0500

    background for about

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -2411,7 +2411,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54230" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61777" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
